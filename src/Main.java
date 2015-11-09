cd0589793d83a6af5c561271b4c5abfaeabcefa1
ryanscottmalmoe <ryanmalmoe@gmail.com>
2015-11-09 12:52:14 -0800

    Iteration 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,39 @@
+import java.io.File;
+import java.util.Iterator;
+import FileWalker.FileWalker;
+import MediaParser.MediaParser;
+import MediaFile.MediaFile;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList <MediaFile> mediaFileArrayList = loadMediaMap();
+
     }
+
+
+    /**
+     *
+     * @param
+     * @return New ArrayList of Media files stored by their filenames
+     */
+    public static ArrayList<MediaFile> loadMediaMap() {
+        ArrayList <MediaFile> mediaFileArrayList = new ArrayList<MediaFile>();
+        FileWalker fw = new FileWalker();
+        Iterator myFiles = fw.listFiles();
+        String fileLocation;
+        while(myFiles.hasNext()) {
+            File file = (File) myFiles.next();
+            fileLocation = file.getAbsolutePath();
+            MediaParser mp = new MediaParser();
+            MediaFile mf = mp.getMediaFileObjectWithFileLocation(fileLocation, file.getName());
+            mf.printFileName();
+            mf.printArtistName();
+            mediaFileArrayList.add(mf);
+        }
+        return mediaFileArrayList;
+    }
+
+
 }
