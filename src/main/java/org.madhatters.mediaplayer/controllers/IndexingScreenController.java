bc576a886644d561fade825a6770bf7b8b57fd4d
Lander Brandt <landerbrandt@gmail.com>
2015-11-30 10:18:53 -0800

    Design/functionality improvements to loading indicator

--- src/main/java/org.madhatters.mediaplayer/controllers/IndexingScreenController.java
+++ src/main/java/org.madhatters.mediaplayer/controllers/IndexingScreenController.java
@@ -1,11 +1,45 @@
 package org.madhatters.mediaplayer.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import org.madhatters.mediaplayer.media.FileFinder;
+import org.madhatters.mediaplayer.models.Mp3;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 /**
  * Created by Lander Brandt on 11/21/15.
  */
 public class IndexingScreenController {
-    
+    @FXML private TextField scanDirTextField;
+    @FXML private Button scanButton;
+    @FXML private Label currentFileLabel;
+    @FXML private ProgressBar fileStatusProgressBar;
+
+    public void initialize() {
+        scanDirTextField.setText(System.getProperty("user.home"));
+
+        scanButton.setOnAction(scanButtonHandler());
+    }
+
+    private EventHandler<ActionEvent> scanButtonHandler() {
+        return new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                scanButton.setDisable(true);
+                fileStatusProgressBar.setProgress(-1.0f);
+
+                Collection<Mp3> files = FileFinder.findIn(scanDirTextField.getText())
+                        .stream()
+                        .map(f -> new Mp3(f.getFileName(), f.getArtistName(), f.getSongTitle(), f.getAlbum()))
+                        .collect(Collectors.toList())
+                ;
+
+                System.out.println(files.size());
+            }
+        };
+    }
 }
